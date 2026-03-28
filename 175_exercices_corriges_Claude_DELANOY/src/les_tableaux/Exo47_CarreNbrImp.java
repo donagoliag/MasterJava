3ad3c83121f2184782ceed659437308f2b680546
donagoliag <donagoliag@gmail.com>
2026-03-28 14:43:59 +0100

    Les tableaux

---
+++ 175_exercices_corriges_Claude_DELANOY/src/les_tableaux/Exo47_CarreNbrImp.java
@@ -0,0 +1,38 @@
+package les_tableaux;
+
+import java.util.Scanner;
+
+public class Exo47_CarreNbrImp {
+
+    public static boolean impair(int x){
+        return x%2 != 0;
+    }
+
+    public static void afficheImpaire(int x){
+        int[] stockimpair = new int[x];
+        int compteur = 0;
+        int j=0;
+        while (compteur<x){
+            if (impair(j)){
+                stockimpair[compteur]=j*j;
+                compteur++;
+            }
+            j++;
+        }
+
+        for (int k = 0; k<stockimpair.length; k++){
+            System.out.println((Math.sqrt(stockimpair[k])+" a pour carre " + stockimpair[k]));
+        }
+
+    }
+
+
+
+    public static void main(String[] args){
+        Scanner nombre = new Scanner(System.in);
+        System.out.println("combien de valeurs : ");
+        int n = nombre.nextInt();
+
+        afficheImpaire(n);
+    }
+}
