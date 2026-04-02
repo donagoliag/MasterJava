f5fe9ef5eac9a2ec51980c2a6aee1086be9ad03c
donagoliag <donagoliag@gmail.com>
2026-04-02 16:12:53 +0100

    CRA MoN USER CONTROLLER

--- cramanager/cramanager/src/main/java/com/cramanager/controller/UserController.java
+++ cramanager/cramanager/src/main/java/com/cramanager/controller/UserController.java
@@ -1,17 +1,51 @@
 package com.cramanager.controller;
 
+import com.cramanager.dto.USER.UpdateRequest;
+import com.cramanager.dto.USER.UserResponseRequest;
+import com.cramanager.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    //Lister tous les users
     @GetMapping
-    public ResponseEntity<String> sayHello(){
-        return ResponseEntity.ok("Hi User");
+    public ResponseEntity<List<UserResponseRequest>> listUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
     }
+
+    //Retrouver un user avec son id
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseRequest> UserInfo(@PathVariable Long id){
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    //modifier un user
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseRequest> majuser (@PathVariable Long id, @RequestBody UpdateRequest request){
+        return ResponseEntity.ok(userService.updateUser(id,request));
+    }
+
+    //Activer/Desactiver un user
+    @PatchMapping("/{id}/toggle")
+    public ResponseEntity<Void> activation(@PathVariable Long id){
+        userService.toggleActive(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //Activer le compte via token
+    @GetMapping("/activate")
+    public ResponseEntity<Void> activeviatoken (@RequestParam String token){
+        userService.activateAccount(token);
+        return ResponseEntity.noContent().build();
+    }
+
 }
