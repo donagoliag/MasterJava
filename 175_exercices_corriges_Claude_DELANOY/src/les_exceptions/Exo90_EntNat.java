fdb64442454922282aad5a631d6c3abe21c17b35
donagoliag <donagoliag@gmail.com>
2026-04-05 11:09:48 +0100

    Exceptions

--- 175_exercices_corriges_Claude_DELANOY/src/les_exceptions/Exo90_EntNat.java
+++ 175_exercices_corriges_Claude_DELANOY/src/les_exceptions/Exo90_EntNat.java
@@ -11,9 +11,18 @@ ErrConst (type classe à définir) lorsque la valeur reçue ne conviendra pas,
 de type EntNat.
 Écrire un petit programme d’utilisation qui traite l’exception ErrConst en affichant un mes-
 sage et en interrompant l’exécution*/
+
+/*Adapter la classe EntNat de l’exercice et le programme d’utilisation de manière à disposer
+dans le gestionnaire d’exception du type ErrConst de la valeur fournie à tort au constructeur.*/
 class ErrConst extends Exception {
-    public ErrConst(String message) {
-        super(message);
+    private int valeur;
+    public ErrConst(int valeur) {
+        super("Valeur negative interdite !");
+        this.valeur = valeur;
+    }
+
+    public int getValeur() {
+        return valeur;
     }
 }
 
@@ -24,7 +33,7 @@ public class Exo90_EntNat {
     public Exo90_EntNat(int n) throws ErrConst{
 
         if(n<0){
-            throw new ErrConst("Valeur negative interdite !");
+            throw new ErrConst(n);
         }
         this.n=n;
     }
@@ -46,6 +55,7 @@ public class Exo90_EntNat {
             System.out.print("Valeur = " + entier.getN());
         }catch (ErrConst e){
             System.out.println("Erreur : " + e.getMessage());
+            System.out.println("Valeur incorrecte : " + e.getValeur());
             System.exit(-1);
         }
 
