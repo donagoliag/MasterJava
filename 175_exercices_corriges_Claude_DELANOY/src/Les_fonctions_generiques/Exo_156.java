e83e5d1ae7bbe60d8263506a9c8c7ce7c9e8c5a7
donagoliag <donagoliag@gmail.com>
2026-04-08 11:01:25 +0100

    GENERICITE

---
+++ 175_exercices_corriges_Claude_DELANOY/src/Les_fonctions_generiques/Exo_156.java
@@ -0,0 +1,24 @@
+package Les_fonctions_generiques;
+
+import java.util.Random;
+
+public class Exo_156 {
+
+    public static  <T> T extrator (T[] tab){
+
+        Random random= new Random();
+        int index = random.nextInt(tab.length);
+
+        return tab[index];
+    }
+
+    public static void main(String[] args){
+
+        String[] nouveau = {"habib","baruk","chap"};
+        Integer[] nbre = {11,4,8,75,6};
+
+        System.out.println(extrator(nouveau));
+        System.out.println(extrator(nbre));
+
+    }
+}
